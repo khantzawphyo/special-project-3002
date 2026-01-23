25672eaf43979b78a95b42615bbcf897365ba46c
mryeminaung <yeminaung013@gmail.com>
2026-01-23 23:05:03 +0630

    feat: add supervisor dashboard demo screen

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -12,6 +12,10 @@ export default function RootLayout() {
 				name="index"
 				options={{ headerShown: false }}
 			/>
+			<Stack.Screen
+				name="dashboard"
+				options={{ headerShown: false }}
+			/>
 		</Stack>
 	);
 }
--- mobile/components/PageWrapper.tsx
+++ mobile/components/PageWrapper.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function PageWrapper({ children }: any) {
-	return <SafeAreaView className="flex-1 px-3">{children}</SafeAreaView>;
+	return <SafeAreaView className="flex-1 bg-slate-50">{children}</SafeAreaView>;
 }
